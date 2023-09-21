0b046e4334f0645d2478706364b8089854bbf90d
Alex Meleshko <meleha.alex@gmail.com>
2023-09-21 17:09:50 +0300

    Create rememberBitmapFromByte function for both platforms

---
+++ shared/src/commonMain/kotlin/com/example/cmpcontacts/core/presentation/BitmapUtil.kt
@@ -0,0 +1,7 @@
+package com.example.cmpcontacts.core.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+
+@Composable
+expect fun rememberBitmapFromByte(bytes: ByteArray?): ImageBitmap?
