5c58f0bff3e7266725a5f304ec15c87635977393
Alex Meleshko <meleha.alex@gmail.com>
2023-09-25 14:19:49 +0300

    Create an App class and implement it for Android

--- androidApp/src/main/java/com/example/cmpcontacts/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/cmpcontacts/android/MainActivity.kt
@@ -3,38 +3,29 @@ package com.example.cmpcontacts.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cmpcontacts.Greeting
+import com.example.cmpcontacts.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+            App(
+                darkTheme = isSystemInDarkTheme(),
+                dynamicColor = false
+            )
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
+    App(
+        darkTheme = isSystemInDarkTheme(),
+        dynamicColor = true
+    )
 }
