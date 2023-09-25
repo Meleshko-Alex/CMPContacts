530e2841efab977cff86a5d50a8a60dd567b06f3
Alex Meleshko <meleha.alex@gmail.com>
2023-09-25 14:20:33 +0300

    Implement App class for iOS

---
+++ shared/src/iosMain/kotlin/com/example/cmpcontacts/MainViewController.kt
@@ -0,0 +1,17 @@
+package com.example.cmpcontacts
+
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIScreen
+import platform.UIKit.UIUserInterfaceStyle
+
+/**
+ * This function is used as a bridge between iOS UI and Compose
+ * **/
+fun MainViewController() = ComposeUIViewController {
+    val isDarkTheme =
+        UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyle.UIUserInterfaceStyleDark
+    App(
+        darkTheme = isDarkTheme,
+        dynamicColor = false
+    )
+}
